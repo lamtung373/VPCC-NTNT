9cd432175280f02006b6204f7d12f424e6e18cda
Lâm Tùng <63940991+lamtung373@users.noreply.github.com>
2025-06-28 22:09:07 +0700

    Update date-calculator.js
    10:08 PM 28/06/2025

--- pages/date-calculator.js
+++ pages/date-calculator.js
@@ -6,7 +6,7 @@ export default function DateCalculatorPage() {
   return (
     <>
       <Head>
-        <title>Tính Thời Hạn Giấy Tờ - VPCC Nguyễn Thị Như Trang - Nguyễn Tùng Lâm</title>
+        <title>Công Cụ Tính Toán Thời Gian - VPCC Nguyễn Thị Như Trang - Nguyễn Tùng Lâm</title>
         <meta name="description" content="Công cụ tính toán thời gian, thời hạn hiệu lực văn bản, ngày làm việc cho văn phòng công chứng" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
